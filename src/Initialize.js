ae650dfddd5e77eb952ad06c6f092a150b74ccbb
Jelocin123 <nicolej.basse123@gmail.com>
2023-08-30 10:49:07 +0200

    Updated

--- src/Initialize.js
+++ src/Initialize.js
@@ -2,17 +2,32 @@ export default class Initialize {
 
   constructor() {
 
-    console.log("Initialize ready");
+      this.weather = {
 
-    this.ninja = document.createElement("div");
-    this.ninja.id = "ninja";
-    document.body.appendChild(this.ninja);
+        author: "The Weatherman",
+        month: ["januar", "februar", "marts", "april", "maj", "juni", "juli"],
+        averageTemperature: [-10, -10, -10, 12, 15, 20, 25, 27],
+        
+        };
 
-    let ninjaImg = document.createElement("img");
-    ninjaImg.src = "./assets/ninja.png";
-    this.ninja.appendChild(ninjaImg);
+      
+        this.container = document.createElement('div');
+        this.container.id = "container";
+        this.container.className = "container";
+        document.body.appendChild(this.container);
 
+        this.container.innerHTML= this.weather.month.map( (item, index)=>{
+
+          return ` 
+          
+          <div class= ${"maps" + index} id='block'>${item}</div>
+          
+                `
+
+        }).join('') + '';
 
 
   } // END constructor
+
+
 } // END class
--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import "../css/style.scss";
 // **  IIFE: Immediately Invoked Function Expression  */
 
 (function () {
-
-  console.log("starting the cool->app");
-
-  let init = new Initialize();
+let init = new Initialize();
 
 })();
